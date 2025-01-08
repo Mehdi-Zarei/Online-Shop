8a76134dc6904500ca281568788370ba205f65ed
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-08 23:12:39 +0330

    Add nodemailer utility for sending verification emails

---
+++ src/utils/nodemailer.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+const configs = require("../configs");
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: configs.nodemailer.user,
+    pass: configs.nodemailer.pass,
+  },
+});
+
+module.exports = {
+  sendVerificationEmail: async (user, subject, text) => {
+    const mailToSend = {
+      from: configs.nodemailer.user,
+      to: user.email,
+      subject,
+      html: text,
+    };
+
+    try {
+      await transporter.sendMail(mailToSend);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+};

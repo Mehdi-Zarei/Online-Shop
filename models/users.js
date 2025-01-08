2c93bbdb7370cb485035fc84e3ac37e84ea15810
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-08 23:11:07 +0330

    Add user and addresses schemas for MongoDB model

---
+++ models/users.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose");
+
+const addressesSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+  },
+  location: {
+    lat: {
+      // طول جغرافیایی
+      type: Number,
+      required: true,
+    },
+    lng: {
+      // عرض جغرافیایی
+      type: Number,
+      required: true,
+    },
+  },
+
+  physicalAddress: {
+    type: String,
+    required: true,
+  },
+
+  cityID: {
+    type: Number,
+    required: true,
+  },
+});
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: false,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    roles: {
+      type: [String],
+      enum: ["OWNER", "ADMIN", "USER", "SELLER", "AUTHOR"],
+      default: ["USER"],
+    },
+    provider: {
+      type: String,
+      enum: ["local", "google"],
+      default: ["local"],
+    },
+    addresses: [addressesSchema],
+
+    avatar: {
+      type: String,
+    },
+    isRestrict: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const user = mongoose.model("user", userSchema);
+
+module.exports = user;

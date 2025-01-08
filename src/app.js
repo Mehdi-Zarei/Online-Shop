bd1445b62a16cf9a5d64417d5b90b2836ad59d9d
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-08 23:11:15 +0330

    Add CORS middleware and authentication routes to Express application

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 
 //* Import Path Files
 
+const { corseOptions } = require("./middlewares/corsOptions");
+const authRoutes = require("./modules/auth/V1/auth.routes");
+
 //* Built-in Middlewares
 
 app.use(express.json());
@@ -13,7 +16,11 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 
 //* Third-party Middleware
 
+app.use(cors(corseOptions));
+
 //* Import Routes
+
+app.use("/api/v1", authRoutes);
 
 //* 404 Error Handler
 

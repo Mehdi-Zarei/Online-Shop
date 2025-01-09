3e9e3be01e921f4b5c07a4e93ce2d4985e909159
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-09 16:25:25 +0330

    Update error handler to return full error details in response message

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const errorHandler = (err, req, res, next) => {
   console.log(err);
   return res.json({
     statusCode: err.status || 500,
-    msg: err.errors[0].message || "Server Error !!",
+    msg: err.errors || "Server Error !!",
   });
 };
 

80c02b18bdd8737428d34a11e5c2327c65915694
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-09 14:19:09 +0330

    Refactor OTP handling in Redis helper functions for improved error handling and clarity

--- src/helpers/redis.js
+++ src/helpers/redis.js
@@ -3,55 +3,75 @@ const redis = require("../redis");
 
 const saveOtpInRedis = async (key, otpCode) => {
   try {
-    const otp = await redis.set(`otp:${key}`, otpCode, "EX", 60);
-
-    return otp;
+    const result = await redis.set(`otp:${key}`, otpCode, "EX", 60);
+    return result;
   } catch (error) {
-    next(error);
+    throw error;
   }
 };
 
-const gettingOtpInfoFromRedis = async (key) => {
-  const otp = await redis.get(`otp:${key}`);
+const gettingOtpFromRedis = async (phone) => {
+  try {
+    const getOTP = await redis.get(`otp:${phone}`);
 
-  console.log("redis otp ->", otp);
-
-  if (!otp) {
-    return {
-      expired: true,
-      remainingTime: 0,
-    };
+    if (!getOTP) {
+      return {
+        remainingTime: 0,
+        expired: true,
+      };
+    }
+    return getOTP;
+  } catch (error) {
+    throw error;
   }
-  const remainingTime = await redis.ttl(`otp:${key}`); // Time in seconds
+};
 
-  const minutes = Math.floor(remainingTime / 60);
+const gettingOtpInfoFromRedis = async (phone) => {
+  try {
+    const remainingTime = await redis.ttl(`otp:${phone}`); // Time in seconds
 
-  const seconds = remainingTime % 60;
+    if (remainingTime <= 0) {
+      return {
+        expired: true,
+        remainingTime: 0,
+      };
+    }
 
-  const formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
-    .toString()
-    .padStart(2, "0")}`;
+    const minutes = Math.floor(remainingTime / 60);
 
-  return {
-    otp,
-    expired: false,
-    remainingTime: formattedTime,
-  };
+    const seconds = remainingTime % 60;
+
+    const formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+
+    return {
+      expired: false,
+      remainingTime: formattedTime,
+    };
+  } catch (error) {
+    throw error;
+  }
 };
 
 const saveRefreshTokenInRedis = async (key, value) => {
-  const refreshToken = await redis.set(
-    `refreshToken:${key}`,
-    value,
-    "EX",
-    configs.redis.refreshTokenExpireTimeInRedis
-  );
+  try {
+    const refreshToken = await redis.set(
+      `refreshToken:${key}`,
+      value,
+      "EX",
+      configs.redis.refreshTokenExpireTimeInRedis
+    );
 
-  return refreshToken;
+    return refreshToken;
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = {
   saveOtpInRedis,
   gettingOtpInfoFromRedis,
   saveRefreshTokenInRedis,
+  gettingOtpFromRedis,
 };

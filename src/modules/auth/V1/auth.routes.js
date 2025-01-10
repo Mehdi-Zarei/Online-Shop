862b9ede5e84496fed6b1c18322b5ed7ad6d55ec
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-10 11:24:20 +0330

    Add routes for password recovery and logout functionality

--- src/modules/auth/V1/auth.routes.js
+++ src/modules/auth/V1/auth.routes.js
@@ -2,21 +2,34 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
+//* Controller
+
 const {
   sent,
   verify,
   getMe,
   login,
   refreshToken,
+  forgetPassword,
+  resetPassword,
+  logOut,
 } = require("./auth.controller");
 
+//* Middlewares
+
 const { bodyValidator } = require("../../../middlewares/validator");
+
+//* Validator Schema
 
 const {
   phoneNumberSchema,
   registerSchema,
   loginSchema,
+  forgetPasswordSchema,
+  resetPasswordSchema,
 } = require("./auth.validators");
+
+//* Routes
 
 router.route("/auth/sent").post(bodyValidator(phoneNumberSchema), sent);
 
@@ -45,5 +58,17 @@ router
 router
   .route("/auth/refresh")
   .get(passport.authenticate("refreshToken", { session: false }), refreshToken);
+
+router
+  .route("/auth/forget-password")
+  .post(bodyValidator(forgetPasswordSchema), forgetPassword);
+
+router
+  .route("/auth/reset-password/:token")
+  .post(bodyValidator(resetPasswordSchema), resetPassword);
+
+router
+  .route("/auth/logout")
+  .post(passport.authenticate("accessToken", { session: false }), logOut);
 
 module.exports = router;

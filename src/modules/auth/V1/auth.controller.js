ebc1532e32796f39bdbac0bd04f2e2e2471a2710
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-10 11:24:11 +0330

    Add password reset functionality with email verification and Redis token management

--- src/modules/auth/V1/auth.controller.js
+++ src/modules/auth/V1/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../../../../models/users");
 
 const bcrypt = require("bcryptjs");
+const uuid = require("uuid").v4;
 
 //* Redis Helper
 const {
@@ -8,6 +9,10 @@ const {
   saveOtpInRedis,
   saveRefreshTokenInRedis,
   gettingOtpFromRedis,
+  saveResetPasswordTokenInRedis,
+  gettingResetPasswordTokenFromRedis,
+  removeResetPasswordTokenFromRedis,
+  removeRefreshTokenFromRedis,
 } = require("../../../helpers/redis");
 
 //* Response Helper
@@ -24,6 +29,10 @@ const {
   generateAccessToken,
   generateRefreshToken,
 } = require("../../../helpers/accessAndRefreshToken");
+
+const { sendVerificationEmail } = require("../../../utils/nodemailer");
+
+const configs = require("../../../../configs");
 
 exports.sent = async (req, res, next) => {
   try {
@@ -169,6 +178,95 @@ exports.refreshToken = async (req, res, next) => {
     const accessToken = generateAccessToken(user._id, user.roles);
 
     return successResponse(res, 200, "OK", { accessToken });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.forgetPassword = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return errorResponse(res, 404, "User Not Found With This Email !!");
+    }
+
+    if (user.isRestrict) {
+      return errorResponse(res, 403, "This User Already Is Banned !!");
+    }
+
+    const resetPasswordToken = uuid();
+
+    const subject = "Reset Password Link";
+
+    const text = `
+    <p>سلام ${user.name} عزیز،</p>
+    <p>درخواست تغییر رمز عبور برای حساب کاربری شما ثبت شده است. اگر این درخواست از سمت شما بوده است، لطفاً از طریق لینک زیر اقدام به تغییر رمز عبور خود کنید:</p>
+    <a href="http://localhost:${configs.server.port}/api/v1/auth/reset-password/${resetPasswordToken}">تغییر رمز عبور</a>
+    <p>این لینک تا <strong>۱ ساعت</strong> آینده معتبر است. اگر شما این درخواست را ارسال نکرده‌اید، لطفاً این ایمیل را نادیده بگیرید یا با پشتیبانی تماس بگیرید.</p>
+    <p>با تشکر،</p>
+    <p>تیم پشتیبانی</p>
+`;
+
+    const sentEmail = await sendVerificationEmail(user, subject, text);
+
+    if (sentEmail) {
+      await saveResetPasswordTokenInRedis(resetPasswordToken, email);
+      return successResponse(res, 200, "Email has been sent successfully.");
+    } else {
+      return errorResponse(
+        res,
+        500,
+        "Failed to send email. Please try again later."
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.resetPassword = async (req, res, next) => {
+  try {
+    const { password } = req.body;
+
+    const { token } = req.params;
+
+    const savedToken = await gettingResetPasswordTokenFromRedis(token);
+
+    if (!savedToken || savedToken.token !== token) {
+      return errorResponse(res, 404, "Invalid or expired reset token.");
+    }
+
+    const hashedNewPassword = bcrypt.hashSync(password, 12);
+
+    const updateUserPassword = await userModel.findOneAndUpdate(
+      { email: savedToken.email },
+      {
+        password: hashedNewPassword,
+      }
+    );
+
+    if (!updateUserPassword) {
+      return errorResponse(res, 404, "User Not Found !!");
+    }
+
+    await removeResetPasswordTokenFromRedis(token);
+
+    return successResponse(res, 200, "Your Password Updated Successfully.");
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.logOut = async (req, res, next) => {
+  try {
+    const user = req.user;
+
+    await removeRefreshTokenFromRedis(user._id);
+
+    return successResponse(res, 200, "User logged out successfully.");
   } catch (error) {
     next(error);
   }

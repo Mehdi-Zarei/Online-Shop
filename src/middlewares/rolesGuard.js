f5d7942c8e2985733f3c47ebcba5d9f96065f8c0
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-17 14:10:02 +0330

    Improve error message for missing token in rolesGuard middleware

--- src/middlewares/rolesGuard.js
+++ src/middlewares/rolesGuard.js
@@ -8,7 +8,11 @@ const rolesGuard = (requiredRoles) => {
       const token = req.headers.authorization?.split(" ")[1];
 
       if (!token) {
-        return res.status(401).json({ message: "No token provided!" });
+        return res
+          .status(401)
+          .json({
+            message: "No token provided!You don't have access to this routes",
+          });
       }
 
       const decoded = jwt.verify(token, configs.auth.jwtSecretAccessToken);

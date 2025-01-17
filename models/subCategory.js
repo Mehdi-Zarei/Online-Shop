6e2fad50085e872f7a49adc68ef8f135b539e9cc
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-17 16:43:06 +0330

    Add subcategory model with schema definition for title, slug, parent, description, and filters

---
+++ models/subCategory.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  slug: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+
+  parent: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+
+  description: {
+    type: String,
+    trim: true,
+  },
+
+  filters: [
+    {
+      name: { type: String, required: true, trim: true },
+      type: { type: String, enum: ["radio", "selectbox"], required: true },
+      options: [{ type: String, default: undefined }],
+    },
+  ],
+});
+
+module.exports = mongoose.model("SubCategory", categorySchema);

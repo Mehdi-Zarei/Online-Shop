ef05e904015ad62c32b809cdc5a4ae9ca1a27779
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-17 14:04:46 +0330

    Update error handler to return error message instead of errors array

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const errorHandler = (err, req, res, next) => {
   console.log(err);
   return res.json({
     statusCode: err.status || 500,
-    msg: err.errors || "Server Error !!",
+    msg: err.message || "Server Error !!",
   });
 };
 

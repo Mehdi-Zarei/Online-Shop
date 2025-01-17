9d7c40dd460e16fcc2b04560c2d10be4e80e7da4
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-17 14:05:16 +0330

    Refactor multer storage utility to include error handling and improve filename generation

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -3,39 +3,41 @@ const fs = require("fs");
 const path = require("path");
 
 exports.multerStorage = (destination, maxSize, allowedTypes = []) => {
-  const fullPath = path.resolve(destination);
-  if (!fs.existsSync(fullPath)) {
-    fs.mkdirSync(fullPath, { recursive: true });
-  }
-
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, destination);
-    },
-
-    filename: function (req, file, cb) {
-      const imageFile = file.originalname.split(".");
-      const extName = imageFile.pop();
-      const fileName = imageFile.join(".");
-
-      cb(null, `${fileName}-${Date.now()}.${extName}`);
-    },
-  });
-
-  const fileFilter = (req, file, cb) => {
-    const ext = path.extname(file.originalname).toLowerCase();
-    if (allowedTypes.length > 0 && allowedTypes.includes(ext)) {
-      cb(null, true);
-    } else {
-      cb(new Error("File type is not allowed"), false);
+  try {
+    const fullPath = path.resolve(destination);
+    if (!fs.existsSync(fullPath)) {
+      fs.mkdirSync(fullPath, { recursive: true });
     }
-  };
 
-  const upload = multer({
-    storage: storage,
-    limits: { fileSize: maxSize * 1024 * 1024 }, // dynamically set max size to MB
-    fileFilter: fileFilter,
-  });
+    const storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, destination);
+      },
 
-  return upload;
+      filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname).toLowerCase();
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(null, `${uniqueSuffix}${ext}`);
+      },
+    });
+
+    const fileFilter = (req, file, cb) => {
+      const ext = path.extname(file.originalname).toLowerCase();
+      if (allowedTypes.length > 0 && allowedTypes.includes(ext)) {
+        cb(null, true);
+      } else {
+        cb(new Error("File type is not allowed"), false);
+      }
+    };
+
+    const upload = multer({
+      storage: storage,
+      limits: { fileSize: maxSize * 1024 * 1024 }, // dynamically set max size to MB
+      fileFilter: fileFilter,
+    });
+
+    return upload;
+  } catch (error) {
+    throw error;
+  }
 };

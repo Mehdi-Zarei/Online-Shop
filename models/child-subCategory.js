fd51b83ada7cbbefef230bc8f24d0cacd753e667
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-19 19:17:40 +0330

    Add timestamps to child subcategory schema and format code for clarity

--- models/child-subCategory.js
+++ models/child-subCategory.js
@@ -1,37 +1,40 @@
 const mongoose = require("mongoose");
 
-const childSubCategorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-
-  slug: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-
-  parent: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "SubCategory",
-    required: true,
-  },
-
-  description: {
-    type: String,
-    trim: true,
-  },
-
-  filters: [
-    {
-      name: { type: String, required: true, trim: true },
-      type: { type: String, enum: ["radio", "selectbox"], required: true },
-      options: [{ type: String, default: undefined }],
+const childSubCategorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-});
+
+    slug: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    parent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "SubCategory",
+      required: true,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    filters: [
+      {
+        name: { type: String, required: true, trim: true },
+        type: { type: String, enum: ["radio", "selectbox"], required: true },
+        options: [{ type: String, default: undefined }],
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("childSubCategory", childSubCategorySchema);

9e1a79057df82f757e0c47b536ecaee6855db7a2
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-19 19:17:35 +0330

    Refactor category schema; add timestamps and virtual field for subcategories

--- models/category.js
+++ models/category.js
@@ -1,35 +1,44 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  slug: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  icon: {
-    type: {
-      filename: { type: String, required: true, trim: true },
-      path: { type: String, required: true, trim: true },
+const categorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
-  },
-  filters: [
-    {
-      name: { type: String, required: true, trim: true },
-      type: { type: String, enum: ["radio", "selectbox"], required: true },
-      options: [{ type: String, default: undefined }],
+    slug: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
-  ],
+    description: {
+      type: String,
+      trim: true,
+    },
+    icon: {
+      type: {
+        filename: { type: String, required: true, trim: true },
+        path: { type: String, required: true, trim: true },
+      },
+    },
+    filters: [
+      {
+        name: { type: String, required: true, trim: true },
+        type: { type: String, enum: ["radio", "selectbox"], required: true },
+        options: [{ type: String, default: undefined }],
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+categorySchema.virtual("subCategories", {
+  ref: "SubCategory", // مدل مرجع
+  localField: "_id", // فیلد محلی (آیدی دسته‌بندی اصلی)
+  foreignField: "parent", // فیلد خارجی (فیلدی در SubCategory که به Category رفرنس می‌دهد)
 });
 
 const Category = mongoose.model("Category", categorySchema);

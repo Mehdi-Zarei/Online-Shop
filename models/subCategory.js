fcf86c5be664fba71f17e77e9bc63ba5ed0f4b16
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-19 19:17:45 +0330

    Add timestamps to subcategory schema and improve code formatting

--- models/subCategory.js
+++ models/subCategory.js
@@ -1,38 +1,41 @@
 const mongoose = require("mongoose");
 
-const subCategorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-
-  slug: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-
-  parent: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
-    required: true,
-  },
-
-  description: {
-    type: String,
-    trim: true,
-  },
-
-  filters: [
-    {
-      name: { type: String, required: true, trim: true },
-      type: { type: String, enum: ["radio", "selectbox"], required: true },
-      options: [{ type: String, default: undefined }],
+const subCategorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-});
+
+    slug: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    parent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    filters: [
+      {
+        name: { type: String, required: true, trim: true },
+        type: { type: String, enum: ["radio", "selectbox"], required: true },
+        options: [{ type: String, default: undefined }],
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 subCategorySchema.virtual("child", {
   ref: "childSubCategory",

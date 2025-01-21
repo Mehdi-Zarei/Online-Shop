f9c405a67e14e770d0569039ade9fa4349c0b6d3
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-21 23:27:03 +0330

    Add product model with schema for product details and seller information

---
+++ models/product.js
@@ -0,0 +1,100 @@
+const mongoose = require("mongoose");
+
+const sellerSchema = new mongoose.Schema(
+  {
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Seller",
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    slug: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    childSubCategory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "childSubCategory",
+      required: true,
+    },
+
+    images: {
+      type: [
+        {
+          type: String,
+          required: true,
+        },
+      ],
+    },
+
+    sellers: {
+      type: [sellerSchema],
+      default: [],
+    },
+
+    filterValues: {
+      type: Map,
+      of: mongoose.Types.Mixed,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+    },
+
+    customFilters: {
+      type: Map,
+      of: String,
+      required: true,
+    },
+
+    shortIdentifier: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;

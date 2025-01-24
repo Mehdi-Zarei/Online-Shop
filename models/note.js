ccafd1b087223df5c130305665d45736ee11d7b6
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-24 19:29:58 +0330

    Add note model to support user-product associations and content storage

---
+++ models/note.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const note = mongoose.model("note", noteSchema);
+
+module.exports = note;

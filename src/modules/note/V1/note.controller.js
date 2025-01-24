95fb108ff06a54f6059e1706d173bd33c3f72472
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-24 19:30:09 +0330

    Add note controller with CRUD operations for note management

---
+++ src/modules/note/V1/note.controller.js
@@ -0,0 +1,29 @@
+const noteModel = require("../../../../models/note");
+
+exports.create = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getAll = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateContent = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.remove = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};

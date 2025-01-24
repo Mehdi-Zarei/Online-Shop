c9f12af7cb413828d451e8d1a15548a74f327399
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-24 19:50:54 +0330

    Add validation for product ID and prevent duplicate notes in creation

--- src/modules/note/V1/note.controller.js
+++ src/modules/note/V1/note.controller.js
@@ -1,7 +1,48 @@
 const noteModel = require("../../../../models/note");
+const productModel = require("../../../../models/product");
+
+const { isValidObjectId } = require("mongoose");
+
+const {
+  errorResponse,
+  successResponse,
+} = require("../../../helpers/responseMessage");
 
 exports.create = async (req, res, next) => {
   try {
+    const { productID, content } = req.body;
+
+    const user = req.user._id;
+
+    if (!isValidObjectId(productID)) {
+      return errorResponse(res, 409, "Product ID Not Valid !!");
+    }
+
+    const isExistProduct = await productModel.findById(productID);
+
+    if (!isExistProduct) {
+      return errorResponse(res, 404, "Product Not Found !!");
+    }
+
+    const isTheNoteRepetitive = await noteModel
+      .findOne({ user }, { productID })
+      .lean();
+
+    if (isTheNoteRepetitive) {
+      return errorResponse(
+        res,
+        409,
+        "Note has already been saved for this product !!"
+      );
+    }
+
+    const newNote = await noteModel.create({
+      user,
+      product: productID,
+      content,
+    });
+
+    return successResponse(res, 201, "New Note Created Successfully.", newNote);
   } catch (error) {
     next(error);
   }

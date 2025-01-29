6d33113e02cc53d0edeb5f8b376240accc36e6e2
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-29 21:10:02 +0330

    Add sellerRequest controller for managing seller requests

---
+++ src/modules/sellerRequest/V1/sellerRequest.controller.js
@@ -0,0 +1,36 @@
+const sellerRequestModel = require("../../../../models/sellerRequests");
+
+exports.getAllSellerRequests = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createSellerRequests = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getOneSellerRequests = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateSellerRequestsStatus = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteSellerRequests = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};

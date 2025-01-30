9a9073f001003bf620e66906a300b3f1036db6c1
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-31 00:05:24 +0330

    Refactor OTP sending logic to use axios for improved error handling and response management

--- src/services/sentOtp.js
+++ src/services/sentOtp.js
@@ -1,33 +1,28 @@
-const request = require("request");
+const axios = require("axios");
 const configs = require("../../configs");
 
 const sentOtp = async (userPhone, otpCode) => {
-  return new Promise((resolve) => {
-    request.post(
-      {
-        url: "http://ippanel.com/api/select",
-        body: {
-          op: "pattern",
-          user: configs.otp.user,
-          pass: configs.otp.pass,
-          fromNum: "3000505",
-          toNum: userPhone,
-          patternCode: configs.otp.pattern,
-          inputData: [{ "verification-code": otpCode }],
-        },
-        json: true,
-      },
-      function (error, response, body) {
-        if (Array.isArray(body) || body.length > 1) {
-          console.log("OTP Error Body -->", body);
-          resolve({ success: false });
-          return;
-        }
+  try {
+    const response = await axios.post("http://ippanel.com/api/select", {
+      op: "pattern",
+      user: configs.otp.user,
+      pass: configs.otp.pass,
+      fromNum: "3000505",
+      toNum: userPhone,
+      patternCode: configs.otp.pattern,
+      inputData: [{ "verification-code": otpCode }],
+    });
 
-        resolve({ success: true });
-      }
-    );
-  });
+    if (Array.isArray(response.data) || response.data.length > 1) {
+      console.log("OTP Error Body -->", response.data);
+      return { success: false };
+    }
+
+    return { success: true };
+  } catch (error) {
+    console.error("OTP Error -->", error);
+    return { success: false };
+  }
 };
 
 module.exports = sentOtp;

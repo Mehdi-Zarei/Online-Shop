dcc691df9aa5775a1d6f4c03d3c6c9d24a6d9292
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-31 14:58:42 +0330

    Add comments router to the application for comment management functionality

--- src/app.js
+++ src/app.js
@@ -13,7 +13,9 @@ const sellerRouter = require("./modules/seller/V1/seller.routes");
 const categoryRouter = require("./modules/categories/V1/category.routes");
 const productsRouter = require("./modules/product/product.routes");
 const notesRouter = require("./modules/note/V1/note.routes");
+const commentRouter = require("./modules/comment/V1/comment.routes");
 const sellerRequestRouter = require("./modules/sellerRequest/V1/sellerRequest.routes");
+
 const {
   redirectToProductPage,
 } = require("./modules/shortLink/V1/shortLink.controller");
@@ -54,6 +56,7 @@ app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/products", productsRouter);
 app.use("/api/v1/notes", notesRouter);
 app.use("/api/v1/seller-requests", sellerRequestRouter);
+app.use("/api/v1/comments", commentRouter);
 app.get("/api/v1/p/:shortIdentifier", redirectToProductPage);
 
 //* 404 Error Handler

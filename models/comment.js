0097a556c54c77f4cf98d9dfe4567c0366b986eb
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-31 14:58:36 +0330

    Add comment and replay schemas for product feedback functionality

---
+++ models/comment.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const replaySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    isAccept: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    isAccept: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    score: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    replay: {
+      type: [replaySchema],
+    },
+  },
+  { timestamps: true }
+);
+
+const comment = mongoose.model("Comment", commentSchema);
+
+module.exports = comment;

4e870265c757ae0fe9a29664fa089f15867a2106
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-31 14:58:51 +0330

    Enhance error handling middleware to manage validation errors and improve response structure

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,9 +1,23 @@
-const errorHandler = (err, req, res, next) => {
-  console.log(err);
-  return res.json({
-    statusCode: err.status || 500,
-    msg: err.message || "Server Error !!",
-  });
-};
+const { errorResponse } = require("../helpers/responseMessage");
 
-module.exports = { errorHandler };
+exports.errorHandler = (err, req, res, next) => {
+  if (err.name === "ValidationError") {
+    const errors = [];
+
+    err.inner.forEach((e) => {
+      errors.push({
+        field: e.path,
+        message: e.message,
+      });
+    });
+
+    console.log({ success: false, error: "Validation Error", data: errors });
+    return errorResponse(res, 400, "Validation Error", errors);
+  }
+
+  let message = err.message || "Internal Server Error";
+  let status = err.status || 500;
+
+  console.log({ success: false, error: message });
+  return errorResponse(res, status, message);
+};

c2dfe85ecca336906b9d2532d78aaedb305c409b
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-31 18:15:40 +0330

    Enhance comment controller with pagination, duplicate checks, and reply management

--- src/modules/comment/V1/comment.controller.js
+++ src/modules/comment/V1/comment.controller.js
@@ -4,6 +4,8 @@ const userModel = require("../../../../models/users");
 const productModel = require("../../../../models/product");
 
 //* Helper Functions
+const { createPagination } = require("../../../helpers/pagination");
+
 const {
   errorResponse,
   successResponse,
@@ -11,6 +13,55 @@ const {
 
 exports.getAllComments = async (req, res, next) => {
   try {
+    const { productID, page = 1, limit = 10 } = req.query;
+
+    if (!isValidObjectId(productID)) {
+      return errorResponse(res, 409, "Product ID Not Valid !!");
+    }
+
+    const mainProduct = await productModel.findById(productID);
+
+    if (!mainProduct) {
+      return errorResponse(res, 404, "Product Not Found With This ID !!");
+    }
+
+    const comments = await commentModel
+      .find({ product: productID, isAccept: true }, "-__v -product")
+      .populate("user", "name")
+      .populate({
+        path: "replies",
+        populate: {
+          path: "user",
+          select: "name",
+        },
+      })
+      .sort({ createdAt: -1 })
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .lean();
+
+    comments.forEach((comment) => {
+      comment.replies = comment.replies.filter(
+        (reply) => reply.isAccept === true
+      );
+    });
+
+    if (comments.length === 0) {
+      return successResponse(
+        res,
+        200,
+        "This Product Don't Have Any Comment Yet !!"
+      );
+    }
+
+    const pagination = createPagination(
+      page,
+      limit,
+      comments.length,
+      "Comments"
+    );
+
+    return successResponse(res, 200, { comments, pagination });
   } catch (error) {
     next(error);
   }
@@ -30,6 +81,16 @@ exports.createComment = async (req, res, next) => {
 
     if (!mainProduct) {
       return errorResponse(res, 404, "Product Not Found !!");
+    }
+
+    const isTheCommentDuplicate = await commentModel.findOne({ content });
+
+    if (isTheCommentDuplicate) {
+      return errorResponse(
+        res,
+        409,
+        "You have already posted another comment with this content."
+      );
     }
 
     const newComment = await commentModel.create({
@@ -58,26 +119,83 @@ exports.editComment = async (req, res, next) => {
 
 exports.removeComment = async (req, res, next) => {
   try {
+    const { commentID } = req.params;
+
+    if (!isValidObjectId(commentID)) {
+      return errorResponse(res, 409, "Comment ID Not Valid !!");
+    }
+
+    const deletedComment = await commentModel.findByIdAndDelete(commentID);
+
+    if (!deletedComment) {
+      return errorResponse(res, 404, "Comment Not Fond with This ID !!");
+    }
+
+    return successResponse(res, 200, "Comment Removed Successfully.");
   } catch (error) {
     next(error);
   }
 };
 
-exports.addReplay = async (req, res, next) => {
+exports.addReply = async (req, res, next) => {
+  try {
+    const { commentID } = req.params;
+    const { content } = req.body;
+    const userID = req.user._id;
+
+    if (!isValidObjectId(commentID)) {
+      return errorResponse(res, 409, "Comment ID Not Valid !!");
+    }
+
+    const isReplyDuplicate = await commentModel.findOne({
+      _id: commentID,
+      replies: {
+        $elemMatch: { content: content, user: userID },
+      },
+    });
+
+    if (isReplyDuplicate) {
+      return errorResponse(
+        res,
+        409,
+        "You have already posted a similar reply to this comment."
+      );
+    }
+
+    const reply = await commentModel.findByIdAndUpdate(
+      commentID,
+      {
+        $push: {
+          replies: {
+            user: userID,
+            content,
+            isAccept: false,
+          },
+        },
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!reply) {
+      return errorResponse(res, 404, "Comment Not Found !!");
+    }
+
+    return successResponse(res, 201, "Reply Comment Created Successfully.", {
+      ReplyComment: reply.replies,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.removeReply = async (req, res, next) => {
   try {
   } catch (error) {
     next(error);
   }
 };
 
-exports.removeReplay = async (req, res, next) => {
-  try {
-  } catch (error) {
-    next(error);
-  }
-};
-
-exports.editReplay = async (req, res, next) => {
+exports.editReply = async (req, res, next) => {
   try {
   } catch (error) {
     next(error);

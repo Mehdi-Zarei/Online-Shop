5842a6fe85bb238ac8d9bf4d4da892b0c8447d9f
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-31 14:58:55 +0330

    Implement comment controller with CRUD operations and error handling

---
+++ src/modules/comment/V1/comment.controller.js
@@ -0,0 +1,48 @@
+exports.getAllComments = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createComment = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.editComment = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.removeComment = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.addReplay = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.removeReplay = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.editReplay = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};

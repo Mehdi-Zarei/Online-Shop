398b5bc560c4f020d1f5b89d6c78a03e985eb0d2
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-31 14:58:59 +0330

    Add comment routes for managing comments and replies with authentication

---
+++ src/modules/comment/V1/comment.routes.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+
+//* Middlewares
+const rolesGuard = require("../../../middlewares/rolesGuard");
+const { bodyValidator } = require("../../../middlewares/validator");
+const passport = require("passport");
+
+//* Validator Schema
+
+//* Routes
+
+router
+  .route("/")
+  .get(getAllComments)
+  .post(
+    passport.authenticate("accessToken", { session: false }),
+    createComment
+  );
+
+router
+  .route("/:commentID")
+  .patch(
+    passport.authenticate("accessToken", { session: false }),
+    rolesGuard(["OWNER", "ADMIN"], editComment)
+  )
+  .delete(
+    passport.authenticate("accessToken", { session: false }),
+    rolesGuard(["OWNER", "ADMIN"]),
+    removeComment
+  );
+
+router
+  .route("/:commentID/replay")
+  .post(passport.authenticate("accessToken", { session: false }), addReplay)
+  .delete(
+    passport.authenticate("accessToken", { session: false }),
+    rolesGuard(["OWNER", "ADMIN"]),
+    removeReplay
+  )
+  .patch(
+    passport.authenticate("accessToken", { session: false }),
+    rolesGuard(["OWNER", "ADMIN"]),
+    editReplay
+  );
+
+module.exports = router;

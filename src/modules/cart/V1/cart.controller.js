08c0dda4b355e9f3c7c63293681526c9bae8954b
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-02-02 18:15:18 +0330

    Add cart controller with basic CRUD operations for managing user cart

---
+++ src/modules/cart/V1/cart.controller.js
@@ -0,0 +1,22 @@
+const cartModel = require("../../../../models/cart");
+
+exports.getCart = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.addToCart = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.removeFromCart = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};

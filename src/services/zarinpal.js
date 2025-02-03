63d9c6e1c9af5593b2984e2e4776876687ddb0ec
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-02-03 21:00:30 +0330

    Add Zarinpal service for payment creation and verification

---
+++ src/services/zarinpal.js
@@ -0,0 +1,48 @@
+const { default: axios } = require("axios");
+
+const configs = require("../../configs");
+
+const zarinpal = axios.create({
+  baseURL: configs.zarinpal.apiBaseUrl,
+});
+
+exports.createPayment = async function ({ amountInRial, description, mobile }) {
+  try {
+    const response = await zarinpal.post("/request.json", {
+      merchant_id: configs.zarinpal.merchantID,
+      callback_url: configs.zarinpal.paymentCallbackUrl,
+      amount: amountInRial,
+      description,
+      metadata: {
+        mobile,
+      },
+    });
+
+    const data = response.data.data;
+
+    return {
+      authority: data.authority,
+      paymentUrl: configs.zarinpal.paymentBaseUrl + data.authority,
+    };
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};
+
+exports.verifyPayment = async function ({ amountInRial, authority }) {
+  try {
+    const response = await zarinpal.post("/verify.json", {
+      merchant_id: configs.zarinpal.merchantID,
+      amount: amountInRial,
+      authority,
+    });
+
+    const data = response.data.data;
+
+    return data;
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};

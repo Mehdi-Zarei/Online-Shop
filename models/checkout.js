530d371921da4f746696aa7b4e2cc409e8b2335b
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-02-05 17:21:22 +0330

    Add total price calculation and enhance shipping address structure in checkout model

--- models/checkout.js
+++ models/checkout.js
@@ -1,29 +1,36 @@
 const mongoose = require("mongoose");
 
-const checkoutItemSchema = new mongoose.Schema({
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-    required: true,
-  },
+const checkoutItemSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
 
-  seller: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Seller",
-    required: true,
-  },
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Seller",
+      required: true,
+    },
 
-  quantity: {
-    type: Number,
-    required: true,
-    min: 1,
-  },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
 
-  priceAtTimeOfPurchase: {
-    type: Number,
-    required: true,
+    priceAtTimeOfPurchase: {
+      type: Number,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const checkoutSchema = new mongoose.Schema(
   {
@@ -35,27 +42,64 @@ const checkoutSchema = new mongoose.Schema(
 
     items: [checkoutItemSchema],
 
-    shippingAddress: {
-      type: String,
-      required: true,
-    },
+    shippingAddress: [
+      {
+        addressName: {
+          type: String,
+          required: true,
+        },
+        postalCode: {
+          type: Number,
+          required: true,
+        },
+        location: {
+          lat: {
+            // طول جغرافیایی
+            type: Number,
+            required: true,
+          },
+          lng: {
+            // عرض جغرافیایی
+            type: Number,
+            required: true,
+          },
+        },
+        provincesID: {
+          type: Number,
+          required: true,
+        },
+
+        cityID: {
+          type: Number,
+          required: true,
+        },
+        physicalAddress: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
 
     authority: {
       type: String,
       unique: true,
       required: true,
     },
-
+    totalCartPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
     expiresAt: {
-      // TTL -> Time To Live
       type: Date,
       required: true,
-      default: () => Date.now() + 60 * 60 * 1000, // 1 Hour from creation
+      default: () => Date.now() + 60 * 60 * 1000,
     },
   },
   { timestamps: true }
 );
 
+//* TTL
 checkoutSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 
 const model = mongoose.model("Checkout", checkoutSchema);

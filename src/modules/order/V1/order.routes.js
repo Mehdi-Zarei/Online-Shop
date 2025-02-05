2bfbf16d79125dc54379176cd9880f33cd6625d9
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-02-05 17:21:48 +0330

    Fix import path for roleGuard middleware in order routes

--- src/modules/order/V1/order.routes.js
+++ src/modules/order/V1/order.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 //* Middleware
-const roleGuard = require("../../../../middlewares/rolesGuard");
+const roleGuard = require("../../../middlewares/rolesGuard");
 const passport = require("passport");
 
 //* Controllers

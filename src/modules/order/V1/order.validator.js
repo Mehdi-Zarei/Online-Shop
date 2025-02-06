a0cad14dff3256752723cd4649c9c4e8beba1c4d
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-02-06 14:38:55 +0330

    Add validation schema for order update

---
+++ src/modules/order/V1/order.validator.js
@@ -0,0 +1,20 @@
+const yup = require("yup");
+
+const updateOrderSchema = yup.object({
+  id: yup.string().required("Order id is required"),
+
+  postTrackingCode: yup
+    .number()
+    .typeError("postTrackingCode must be a number")
+    .required("postTrackingCode is required"),
+
+  status: yup
+    .string()
+    .oneOf(
+      ["PROCESSING", "SHIPPED", "DELIVERED"],
+      "Invalid status value!!status value must be one of the 'PROCESSING' 'SHIPPED' 'DELIVERED'"
+    )
+    .required("status is required"),
+});
+
+module.exports = { updateOrderSchema };

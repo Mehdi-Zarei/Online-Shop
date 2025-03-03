467204cf8ba79a786263d39f6f30075410236348
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-03-03 21:27:47 +0330

    Update refresh token extraction to use cookies instead of authorization header

--- src/strategies/passport-refreshToken.js
+++ src/strategies/passport-refreshToken.js
@@ -1,28 +1,36 @@
+// const jwtStrategy = require("passport-jwt").Strategy;
+// const { ExtractJwt } = require("passport-jwt");
+// const configs = require("../../configs");
+// const userModel = require("../../models/users");
+// const bcrypt = require("bcryptjs");
+// const { getRefreshTokenFromRedis } = require("../helpers/redis");
+
 const jwtStrategy = require("passport-jwt").Strategy;
-const { ExtractJwt } = require("passport-jwt");
+const bcrypt = require("bcryptjs");
 const configs = require("../../configs");
 const userModel = require("../../models/users");
-const bcrypt = require("bcryptjs");
 const { getRefreshTokenFromRedis } = require("../helpers/redis");
 
 module.exports = new jwtStrategy(
   {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    jwtFromRequest: (req) => req.cookies?.refreshToken,
     secretOrKey: configs.auth.jwtSecretRefreshToken,
     passReqToCallback: true,
   },
   async (req, payload, done) => {
     try {
-      const refreshToken = ExtractJwt.fromAuthHeaderAsBearerToken()(req);
+      const refreshToken = req.cookies.refreshToken;
+
+      if (!refreshToken) {
+        return done(null, false);
+      }
 
       const user = await userModel.findOne({ _id: payload.id }, "-password");
-
       if (!user) {
         return done(null, false);
       }
 
       const hashedRefreshToken = await getRefreshTokenFromRedis(user._id);
-
       if (!hashedRefreshToken) {
         return done(null, false);
       }
